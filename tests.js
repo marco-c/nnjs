73a54ab452dc63a8a7a60f4cbac517ab5ce35ee7
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-13 17:08:02 +0200

    Add script to run all tests

---
+++ tests.js
@@ -0,0 +1,15 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+load('testNOT.js');
+load('testNOTWithReLU.js');
+load('testNOTTwoLayers.js');
+load('testOR.js');
+load('testORTwoLayers.js');
+load('testAND.js');
+load('testXOR.js');
+load('testVectorNOT.js');
+load('testIRIS.js');
+load('testMNIST.js');
