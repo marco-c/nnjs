7cc53a7055c3357476a854bf9344cf7ac2cd6032
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-24 22:29:58 +0200

    Update mnist.py script to generate JSON files; update MNIST .gitignore

--- data/mnist/mnist.py
+++ data/mnist/mnist.py
@@ -7,33 +7,29 @@ f.close()
 train_set_images = train_set[0]
 train_set_labels = train_set[1]
 
-def generateImagesVar(varName, imageSet):
-  gen = "var " + varName + " = ["
-
+def generateImagesVar(imageSet):
+  images = []
   for i in range(0,len(imageSet)):
-    gen += "["
+    image = []
     for j in range(0,len(imageSet[i])):
-      gen += str(imageSet[i][j]) + ","
-    gen += "],"
+      image.append(str(imageSet[i][j]))
 
-  gen += "];"
+    images.append("[" + ",".join(image) + "]")
 
-  return gen
+  return "[" + ",".join(images) + "]"
 
-def generateLabelsVar(varName, labelSet):
-  gen = "var " + varName + " = ["
+def generateLabelsVar(labelSet):
+  labels = []
 
   for i in range(0,len(labelSet)):
-    gen += str(labelSet[i]) + ","
+    labels.append(str(labelSet[i]))
 
-  gen += "];"
+  return "[" + ",".join(labels) + "]"
 
-  return gen
-
-open('mnist_train_images.js', 'w').write(generateImagesVar("trainImages", train_set[0]))
-open('mnist_test_images.js', 'w').write(generateImagesVar("testImages", test_set[0]))
-open('mnist_train_labels.js', 'w').write(generateLabelsVar("trainLabels", train_set[1]))
-open('mnist_test_labels.js', 'w').write(generateLabelsVar("testLabels", test_set[1]))
+open('mnist_train_images.json', 'w').write(generateImagesVar(train_set[0]))
+open('mnist_test_images.json', 'w').write(generateImagesVar(test_set[0]))
+open('mnist_train_labels.json', 'w').write(generateLabelsVar(train_set[1]))
+open('mnist_test_labels.json', 'w').write(generateLabelsVar(test_set[1]))
 
 print(len(train_set))
 print(len(train_set[0]))
