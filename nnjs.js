c54f0b522daa0eb9fdce0f12cbe38be663df0efd
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-24 22:26:39 +0200

    Use ES6 modules; temporarily add some polyfills in nnjs.js

--- nnjs.js
+++ nnjs.js
@@ -3,8 +3,104 @@
 
 'use strict';
 
-load('rand.js');
-load('util.js');
+import Rand from './rand';
+import Util from './util';
+
+// XXX: Move polyfills somewhere else
+if (!Float64Array.prototype.fill) {
+  Float64Array.prototype.fill = function(value, start=0, end=this.length) {
+    for (var i = start; i < end; i++) {
+      this[i] = value;
+    }
+  };
+}
+
+if (!Float64Array.prototype.findIndex) {
+  Float64Array.prototype.findIndex = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Float64Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
+if (!Float64Array.prototype.every) {
+  Float64Array.prototype.every = function(callbackfn, thisArg) {
+    'use strict';
+    var T, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling ToObject passing the this
+    //    value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal method
+    //    of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
+    if (typeof callbackfn !== 'function') {
+      throw new TypeError();
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
+
+    // 6. Let k be 0.
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while (k < len) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal
+      //    method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal method
+        //    of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Let testResult be the result of calling the Call internal method
+        //     of callbackfn with T as the this value and argument list
+        //     containing kValue, k, and O.
+        var testResult = callbackfn.call(T, kValue, k, O);
+
+        // iii. If ToBoolean(testResult) is false, return false.
+        if (!testResult) {
+          return false;
+        }
+      }
+      k++;
+    }
+    return true;
+  };
+}
 
 var Blob = function(width, height, depth, num=1) {
   this.width = width;
@@ -501,3 +597,6 @@ Trainer.prototype.test = function(testVectors, testLabels) {
 
   return correct / testVectors.length;
 };
+
+export default { Blob, ConvolutionLayer, PoolingLayer, ReLULayer, SigmoidLayer, LinearLayer,
+                 RegressionLayer, SoftmaxLayer, Network, Trainer };
--- util.js
+++ util.js
@@ -1,24 +1,17 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+function argmax(array) {
+  return array.findIndex((elem) => array.every(other => other <= elem));
+}
 
-'use strict';
-
-var Util = (function() {
-  function argmax(array) {
-    return array.findIndex((elem) => array.every(other => other <= elem));
+function shuffle(array) {
+  for (var i = array.length - 1; i >= 1; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var tmp = array[j];
+    array[j] = array[i];
+    array[i] = tmp;
   }
+}
 
-  function shuffle(array) {
-    for (var i = array.length - 1; i >= 1; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tmp = array[j];
-      array[j] = array[i];
-      array[i] = tmp;
-    }
-  }
-
-  return {
-    argmax: argmax,
-    shuffle: shuffle,
-  };
-})();
+export default {
+  argmax,
+  shuffle,
+};
